ed0918b8a18f56a6466e748dcc7d2123d20c3111
rmuehe <rmuehe@gmail.com>
2023-11-24 16:23:44 +0200

    Compile HelloJava & Add Comments to HelloJava.java

--- section1/HelloJava.java
+++ section1/HelloJava.java
@@ -1,4 +1,9 @@
 public class HelloJava {
+    // javac HelloJava.java 
+    // compiles to HelloJava.class in bytecode
+    
+    // java HelloJava 
+    // runs that bytecode
     public static void main(String[] args) {
         System.out.println("Hello Java");
     }
