8de9cc97656e0a020235a4ca8a5bb43f0410a0b6
rmuehe <rmuehe@gmail.com>
2023-11-24 16:14:00 +0200

    Add HelloJava.java to section1

--- section1/HelloJava.java
+++ section1/HelloJava.java
@@ -1,5 +1,5 @@
-class HelloJava {
-  public static void main(String[] args) {
-    System.out.println("Hello, Java");
-  }
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
 }
